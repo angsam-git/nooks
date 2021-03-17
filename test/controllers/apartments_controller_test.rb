d7ba0a3298c91e83f0c547194281ec1bf133382c
Alaz Sengul <alazsengul@gmail.com>
2021-03-17 02:05:39 -0400

    deleted test:unit

--- test/controllers/apartments_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ApartmentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get apartments_index_url
-    assert_response :success
-  end
-end
