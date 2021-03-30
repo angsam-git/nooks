e440cb66a6e9618c53e0e778e910feefc93f14f8
Cameron Miller <cm3959@columbia.edu>
2021-03-29 20:48:40 -0400

    working on iter2

--- spec/controllers/apartments_controller_spec.rb
+++ spec/controllers/apartments_controller_spec.rb
@@ -7,11 +7,23 @@ describe ApartmentsController, type: :controller do
 		let!(:apt2) { FactoryBot.create(:Apartment, address: '61 E 77st St, New York, NY 10075', rent: 6981.0, beds: 1, bath: 2, sqft: 49000) }
 
 
+	    context 'A group created a new group id'
+			it 'there should exist a corresponding route' do
+				expect(get("/id")).to route_to("pages#id")
+			end
+
+			it 'and invalid id\'s should not receive a corresponding route' do
+  				visit("/invalid")
+  				expect(page.status_code).to be(404)
+			end
+		end
+
 		context 'An apartment listing has been added to the DB' do
-	      		it 'should appear on the apartments page' do
-	        		expect(page).to have_content(apt1.title)
-	      		end
-	    	end
-		
+	      	it 'should appear on the apartments page' do
+	      		visit("/apartments")
+	        	expect(page).to have_content(apt1.title)
+	      	end
+	    end
+
 	end
 end
