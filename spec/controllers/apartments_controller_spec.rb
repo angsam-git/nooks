cafff79cdf27abddf4f74d58f0b74a652ad1ec46
Cameron Miller <cm3959@columbia.edu>
2021-03-29 21:35:51 -0400

    iter 2

--- spec/controllers/apartments_controller_spec.rb
+++ spec/controllers/apartments_controller_spec.rb
@@ -3,18 +3,24 @@ require 'rails_helper'
 describe ApartmentsController, type: :controller do
 	describe "Apartments" do
 		
-		let!(:apt1) { FactoryBot.create(:Apartment, address: '4 E 51st St, New York, NY 10022', rent: 6170.0, beds: 2, bath: 2, sqft: 150000) }
-		let!(:apt2) { FactoryBot.create(:Apartment, address: '61 E 77st St, New York, NY 10075', rent: 6981.0, beds: 1, bath: 2, sqft: 49000) }
+		let!(:apt1) { FactoryBot.create(:Apartment, address: '4 E 51st St, New York, NY 10022', rent: 6170.0, beds: 2, bath: 2, sqft: 150000, groupid: "id1") }
+		let!(:apt2) { FactoryBot.create(:Apartment, address: '61 E 77st St, New York, NY 10075', rent: 6981.0, beds: 1, bath: 2, sqft: 49000, groupid: "id2") }
 
 
 	    context 'A group created a new group id'
 			it 'there should exist a corresponding route' do
-				expect(get("/id")).to route_to("pages#id")
+				expect(get("/id1")).to route_to("pages#id1")
 			end
 
-			it 'and invalid id\'s should not receive a corresponding route' do
+			it 'invalid id\'s should not receive a route' do
   				visit("/invalid")
   				expect(page.status_code).to be(404)
+			end
+
+			it 'each group should only see their own listings, not other groups\' listings' do
+				visit("/id1")
+	        	expect(page).to have_content(apt1.title)
+	        	expect(page).not_to have_content(apt2.title)
 			end
 		end
 
@@ -22,6 +28,7 @@ describe ApartmentsController, type: :controller do
 	      	it 'should appear on the apartments page' do
 	      		visit("/apartments")
 	        	expect(page).to have_content(apt1.title)
+	        	expect(page).to have_content(apt2.title)
 	      	end
 	    end
 
---
+++ spec/controllers/groups_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe GroupsController, type: :controller do
+	describe "Groups" do
+		
+	end
+end
